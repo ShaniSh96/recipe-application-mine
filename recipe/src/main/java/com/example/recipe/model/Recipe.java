c88a7e2d61157138f889af8d88443cb3df189135
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-28 15:31:02 +0530

    Many to One on recipe and ingredient

--- recipe/src/main/java/com/example/recipe/model/Recipe.java
+++ recipe/src/main/java/com/example/recipe/model/Recipe.java
@@ -1,11 +1,14 @@
 package com.example.recipe.model;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -22,6 +25,10 @@ public class Recipe {
 	private String source;
 	private String url;
 	private String directions;
+	
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
+	
 	
 	//private Difficulty difficulty;
 	@Lob
@@ -89,6 +96,12 @@ public class Recipe {
 	}
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 	
 	
