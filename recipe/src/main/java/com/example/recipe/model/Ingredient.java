53e4940000ff9b07bb692d1d243636b83c96b865
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-28 15:39:27 +0530

    one to many jpa assignment

--- recipe/src/main/java/com/example/recipe/model/Ingredient.java
+++ recipe/src/main/java/com/example/recipe/model/Ingredient.java
@@ -3,10 +3,12 @@ package com.example.recipe.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Ingredient {
@@ -19,7 +21,10 @@ public class Ingredient {
 	private String description;
 	private BigDecimal amount;
 
-	//private UnitOfMeasure uom;
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
+	
+	
 	@ManyToOne
 	private Recipe recipe;
 
