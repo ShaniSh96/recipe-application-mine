7d5e3789b18bef55a89d7856db59069bfcb6359f
Shivani Shanishchara <sshanishchara@gmail.com>
2020-07-29 12:10:22 +0530

    Spring Data JPA

--- recipe/src/main/java/com/example/recipe/repository/UnitOfMeasureRepository.java
+++ recipe/src/main/java/com/example/recipe/repository/UnitOfMeasureRepository.java
@@ -1,9 +1,13 @@
 package com.example.recipe.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.example.recipe.model.UnitOfMeasure;
 
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long>{
 
+	Optional<UnitOfMeasure> findByUom(String uom);
+	
 }
